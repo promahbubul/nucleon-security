d222b013c565c0ef52ebeca09363831643041848
promahbubul <mahbublalam500@gmail.com>
2024-02-07 18:37:13 +0600

    capacities page update

--- src/pages/Capacity/Capacity.jsx
+++ src/pages/Capacity/Capacity.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Capacity = () => {
   return (
-    <div>Capacity</div>
-  )
-}
+    <div>
+      <h1 className="text-center text-3xl font-extrabold text-white underline">
+        Capacity
+      </h1>
+    </div>
+  );
+};
 
-export default Capacity
+export default Capacity;
