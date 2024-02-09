f54006594aec13329f330ca9b8b00ffb74340bb0
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-02-09 23:00:54 +0600

    event page dark mode update

--- src/components/Events/EventTable.jsx
+++ src/components/Events/EventTable.jsx
@@ -45,11 +45,11 @@ const EventTable = () => {
     <div className="mt-3">
       <SectionContainer icon={FaList} title={"Events"}>
         {/* HEAD */}
-        <div className="flex flex-row bg-white ">
+        <div className="flex flex-row bg-white dark:bg-navy-800 ">
           {/* HEAD LEFT  */}
           <div className="w-6/12  grid grid-cols-12  items-center gap-2 ">
             <select
-              className="py-1 col-span-3 h-full cursor-pointer px-2 rounded-md border border-secondary bg-slate-200"
+              className="py-1 col-span-3 h-full cursor-pointer px-2 rounded-md border border-gray-600  bg-gray-200 dark:bg-navy-500   text-navy-100"
               disabled
             >
               <option value="timestamp" className="" selected>
@@ -60,17 +60,17 @@ const EventTable = () => {
               type="date"
               name=""
               id=""
-              className="col-span-8 border px-2 h-full rounded-sm outline-none border-slate-300"
+              className="col-span-8 rounded-md px-2 h-full outline-none  bg-gray-200 dark:bg-navy-500   text-navy-100"
             />
           </div>
           {/* HEAD RIGHT */}
-          <div className="flex flex-row justify-end gap-2 text-sm BORDER w-6/12">
+          <div className="flex flex-row justify-end gap-2 text-sm  w-6/12">
             <Button icon={IoSearch} className={"bg-info py-2 px-4"}>
               Search
             </Button>
             <Button
               className={
-                "bg-gray-200 p-2 text-sm border border-gray-300 hover:bg-gray-100  duration-300  "
+                " bg-gray-200 dark:bg-navy-500   text-navy-100 p-2 text-sm    duration-300  "
               }
             >
               5 / 5 results
@@ -78,7 +78,7 @@ const EventTable = () => {
             <Button
               icon={FaFilter}
               className={
-                "bg-gray-200 p-2 text-sm border border-gray-300 hover:bg-gray-100  duration-300  "
+                " bg-gray-200 dark:bg-navy-500   text-navy-100 p-2 text-sm    duration-300  "
               }
             >
               Add Filter
@@ -89,7 +89,7 @@ const EventTable = () => {
             >
               <button
                 onClick={() => setShowDropdown(!showDropdown)}
-                className="bg-gray-200 text-sm p-2 rounded-md border border-gray-300 flex flex-row gap-2 items-center"
+                className=" bg-gray-200 dark:bg-navy-500  text-gray-500 dark:text-navy-100 text-sm p-2 rounded-md  flex flex-row gap-2 items-center"
               >
                 <MdViewList className="text-lg" />
                 <span className="">List View</span>
@@ -103,54 +103,56 @@ const EventTable = () => {
             </div>
             <Button
               icon={FaDownload}
-              className={"bg-gray-200 p-2 text-sm border border-gray-300"}
+              className={
+                "bg-gray-200 dark:bg-navy-500  text-gray-500 dark:text-navy-100  p-2 text-sm "
+              }
             ></Button>
           </div>
         </div>
         {/* TABLE */}
         <table className="mt-3">
           <div className="tbody">
-            <tr className=" grid grid-cols-12 text-secondary text-base font-bold text-left border-b border-b-slate-200 mt-3 ">
-              <th className=" col-span-2  hover:text-black cursor-pointer">
+            <div className=" grid grid-cols-12 text-gray-500 dark:text-navy-100  text-base font-bold text-left border-b-2 pb-2 border-b-gray-600 mt-3 ">
+              <h3 className=" col-span-2  hover:text-gray-600 dark:hover:text-slate-100 cursor-pointer">
                 Date
-              </th>
-              <th className=" col-span-1  hover:text-black cursor-pointer">
+              </h3>
+              <h3 className=" col-span-1  hover:text-gray-600 dark:hover:text-slate-100 cursor-pointer">
                 Host
-              </th>
-              <th className=" col-span-1  hover:text-black cursor-pointer">
+              </h3>
+              <h3 className=" col-span-1  hover:text-gray-600 dark:hover:text-slate-100 cursor-pointer">
                 Type
-              </th>
-              <th className=" col-span-4  hover:text-black cursor-pointer">
+              </h3>
+              <h3 className=" col-span-4  hover:text-gray-600 dark:hover:text-slate-100 cursor-pointer">
                 Process
-              </th>
-              <th className=" col-span-2  hover:text-black cursor-pointer">
+              </h3>
+              <h3 className=" col-span-2  hover:text-gray-600 dark:hover:text-slate-100 cursor-pointer">
                 Target
-              </th>
-              <th className=" col-span-2  hover:text-black cursor-pointer">
+              </h3>
+              <h3 className=" col-span-2  hover:text-gray-600 dark:hover:text-slate-100 cursor-pointer">
                 Action
-              </th>
-            </tr>
+              </h3>
+            </div>
             {events.map((event, index) => (
               <tr
                 key={index}
-                className="last:border-b-0  duration-300  text-base border-b border-b-slate-200 text-black relative"
+                className="last:border-b-0  duration-300  text-base border-b border-b-slate-600 text-black relative"
               >
                 <input
                   type="checkbox"
                   className="absolute top-0 right-0 z-10 opacity-0  peer h-10 w-full cursor-pointer
                       "
                 />
-                <FaChevronDown className="absolute right-6 top-3 peer-checked:rotate-180" />
+                <FaChevronDown className="absolute dark:text-navy-100 text-gray-500 right-6 top-3 peer-checked:rotate-180" />
 
-                <div className="grid grid-cols-12 hover:bg-[#ececec]  py-2 ">
-                  <td className="col-span-2">
+                <div className="grid grid-cols-12 text-gray-500 dark:text-navy-100 hover:bg-navy-700  py-2 ">
+                  <p className="col-span-2">
                     {event.date} <span className="">{event.time}</span>
-                  </td>
-                  <td className="col-span-1">{event.host}</td>
-                  <td className="col-span-1">{event.type}</td>
-                  <td className="col-span-4">{event.process}</td>
-                  <td className="col-span-2">{event.target}</td>
-                  <td className="col-span-2 flex flex-row gap-2 items-center">
+                  </p>
+                  <p className="col-span-1">{event.host}</p>
+                  <p className="col-span-1">{event.type}</p>
+                  <p className="col-span-4">{event.process}</p>
+                  <p className="col-span-2">{event.target}</p>
+                  <p className="col-span-2 flex flex-row gap-2 items-center">
                     {event.action == "Allowed" ? (
                       <FaCheck className="text-xl font-bold text-success" />
                     ) : event.action == "Blocked" ? (
@@ -159,14 +161,14 @@ const EventTable = () => {
                       <IoWarning className="text-xl font-bold text-warning" />
                     )}
                     {event.action}
-                  </td>
+                  </p>
                 </div>
-                <div className="hidden flex-row gap-2 peer-checked:flex bg-[#ececec] py-2 px-3">
+                <div className="hidden flex-row gap-2 peer-checked:flex bg-slate-200 dark:bg-navy-900 py-2 px-3">
                   {/* event menu */}
                   <div className=" flex flex-col gap-2">
                     {eventsMenu.map((menu, i) => (
                       <Link
-                        className="p-2  w- bg-white rounded-md text-xl"
+                        className="p-2  w- dark:text-slate-300 text-gray-500 dark:bg-navy-600 rounded-md text-xl"
                         key={i}
                         to={menu.path}
                       >
