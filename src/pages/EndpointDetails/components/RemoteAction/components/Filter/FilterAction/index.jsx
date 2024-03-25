8b0aefd8faa3e739e9ab6d8a82c22f62dcbda708
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-03-25 15:21:50 +0600

    remote new action modal update

--- src/pages/EndpointDetails/components/RemoteAction/components/Filter/FilterAction/index.jsx
+++ src/pages/EndpointDetails/components/RemoteAction/components/Filter/FilterAction/index.jsx
@@ -10,8 +10,12 @@ import {
 import OffButton from "@shared/components/Buttons/OffButton";
 import { Tooltip } from "react-tooltip";
 import Button from "@shared/components/Buttons/Button/Button";
+import { useState } from "react";
+import OnButton from "../../../../../../../shared/components/Buttons/OnButton";
 
 const FilterAction = ({ showModal, handleModal }) => {
+  const [toggleNotificationButton, setToggleNotificationButton] =
+    useState(false);
   return (
     <Modal showModal={showModal} handleModal={handleModal}>
       <SectionContainer
@@ -27,18 +31,20 @@ const FilterAction = ({ showModal, handleModal }) => {
           <div className="flex flex-col items-center justify-center">
             <div className="h-8 w-0.5 bg-info "></div>
             <div className="p-2 border w-max border-info rounded-full">
-              <FaFileDownload className="text-sm" />
+              <FaFileDownload className="text-sm dark:text-navy-100" />
             </div>
             <div className="h-64 w-0.5 bg-info "></div>
             <div className="p-2 border w-max cursor-pointer border-info hover:shadow-md rounded-full">
-              <FaPlus className="text-sm hover:text-info duration-300 " />
+              <FaPlus className="text-sm hover:text-info dark:text-navy-100 duration-300 " />
             </div>
           </div>
           {/* right box */}
           <div className="border w-full p-2 flex flex-col gap-2">
             {/* TYPE */}
             <div className="flex flex-row">
-              <h4 className="w-4/12 text-base font-bold text-navy-700">Type</h4>
+              <h4 className="w-4/12 text-base font-bold text-navy-700 dark:text-navy-100">
+                Type
+              </h4>
               <select className="w-8/12 border outline-none border-slate-300 rounded-sm  p-1">
                 <option>Download File</option>
                 <option>Kill process </option>
@@ -48,7 +54,7 @@ const FilterAction = ({ showModal, handleModal }) => {
             </div>
             {/* NOTIFICATION */}
             <div className="flex flex-row pb-5 border-b border-b-slate-300">
-              <h4 className="w-4/12 flex flex-row items-center gap-1     text-base font-bold text-navy-700">
+              <h4 className="w-4/12 flex flex-row items-center gap-1  dark:text-navy-100   text-base font-bold text-navy-700">
                 Notifications
                 <span id="notification" className="cursor-pointer">
                   <FaInfoCircle />
@@ -66,18 +72,27 @@ const FilterAction = ({ showModal, handleModal }) => {
                 />
               </h4>
               <div className="w-8/12">
-                <OffButton />
+                {toggleNotificationButton ? (
+                  <OnButton
+                    className={"bg-info"}
+                    handleButton={() => setToggleNotificationButton(false)}
+                  />
+                ) : (
+                  <OffButton
+                    handleButton={() => setToggleNotificationButton(true)}
+                  />
+                )}
               </div>
             </div>
             {/* REBOOT */}
             <div className="flex flex-row items-center gap-2">
               <div className="w-10 h-0.5 bg-slate-300"></div>
-              <p className="text-base font-normal text-navy-700">
+              <p className="text-base font-normal text-navy-700 dark:text-navy-100">
                 Requested files will be downloaded from the selected endpoint
               </p>
             </div>
             <div className="flex flex-row items-start pb-5 mt-2 border-b border-b-slate-300">
-              <h4 className="w-4/12 flex flex-row items-center gap-1     text-base font-bold text-navy-700">
+              <h4 className="w-4/12 flex flex-row items-center gap-1     text-base font-bold text-navy-700 dark:text-navy-100">
                 Reboot
               </h4>
               <div className="w-8/12">
